c538a259ec59cf0982a11d57037cf75cca38be08
ombasaabuya <ombasabuya@gmail.com>
2023-01-25 16:55:27 +0300

    added src/components/PetCard.js

--- src/components/PetCard.js
+++ src/components/PetCard.js
@@ -1,13 +1,23 @@
 import React from "react";
+// eslint-disable-next-line no-undef
+console.log(props)
 
+function PetCard(props) {
 
-function PetCard(){
+    return (
+        <div className="card">
+            <h2>{props.name}</h2>
+            <img src={props.image}alt={props.name} />
+                <div className="Pet-info">
+                    <h3>Pet Description</h3>
+                    <ul>
+                        {props.description}
+                    </ul>
 
-    return(
-        <div>
-            
-        </div>
-    )
-}
+                </div>
+                </div>
+                );
+    }
 
 export default PetCard
+
