896fae9e1f799a05cf7ddfc697f4b545d97298ff
evance-14 <kiropevans097@gmail.com>
2023-01-26 10:57:31 +0300

    added a pet delete function

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,25 @@
-import React from "react";
+import React,{useState} from "react";
 import NavBar from "./NavBar";
 import About from "./About";
 import DisplayLostPet from "./DisplayLostPet";
 import Form from "./Form";
 
 function Home(){
+    const [pets,setPets] = useState([]);
+
+    const handleDelete = (id) =>{
+       setPets(pets.filter((pet) =>pet.id !== id));
+    };
 
     return(
         <div>
             <>Home</>
             <NavBar />
             <About />
-            <DisplayLostPet />
+            <DisplayLostPet  onDelete={handleDelete}/>
             <Form />
         </div>
     )
 }
 
-export default Home
+export default Home;
