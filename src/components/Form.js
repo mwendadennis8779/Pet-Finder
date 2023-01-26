221a308a65e0aa0c0fe46b9206164bf19503a307
Dennis <mwendadennis8779@gmail.com>
2023-01-26 10:52:49 +0300

    Complete Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,52 @@
-import React from "react";
+import React, {useState, useRef} from "react";
 
 
-function Form(){
+
+
+function Form({pets, setPets}){
+    const [formData, setFormData] = useState({
+        image: '',
+        contact: '',
+        description: '',
+    
+    })
+    
+    const newForm = useRef(null)
+    
+    function handleChange(e){
+        const name = e.target.name;
+        const value = e.target.value;
+    
+        setFormData({...formData, [name]:value})
+    }
+     function handleSubmit(e){
+        fetch('http://localhost:3000/characters', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+        })
+        .then(res => res.json())
+        .then(data => {
+            const newLostPet = [...pets, data]
+            console.log(data)
+            setPets(newLostPet)
+        })
+
+        
+     }
+    
 
     return(
         <div>
             <p>Form</p>
+            <form onClick={handleSubmit} id ="form" ref={newForm}>
+                <input  placeholder="image" name="image" onChange={handleChange}/>
+                <input  placeholder="contact " name="contact" onChange={handleChange}/>
+                <textarea placeholder="description" name="description" rows={7} onChange={handleChange}/>
+                <input  type="submit" value="upload pet"/>
+                
+            </form>
         </div>
     )
 }
